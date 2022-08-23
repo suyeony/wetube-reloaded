294c5b0b964352c83e8e1ad1d429df41d30b2acb
yangsuyeon <suyeonyang03@gmail.com>
2022-08-23 17:43:39 +0900

    video function added

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
 
 // global variable volumeValue
 let volumeValue = 0.5;
@@ -50,7 +54,42 @@ const handleVolumeChange = (event) => {
    
 }
 
+const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substring(14, 19);
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
+}
+
+const handleTimeUpdate = () => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+}
+
+const handleTimelineChange = (event) => {
+    const {
+        target: { value }
+    } = event;
+    video.currentTime = value;
+}
+
+const handleFullScreen = () => {
+    const fullscreen = document.fullscreenElement
+    if (fullscreen) {
+        document.exitFullscreen();
+        fullScreenBtn.innerText = "Enter fullscreen";
+    }
+    else {
+        videoContainer.requestFullscreen();
+        fullScreenBtn.innerText = "Exit fullscreen";
+    }
+    
+}
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata); 
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullScreen);
